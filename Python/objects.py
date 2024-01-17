02599f879c233453fd2028b76b592b2f2d65aa81
Devin <djthomas1126@gmail.com>
2024-01-16 19:56:41 -0600

    initialized Paddle class

---
+++ Python/objects.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Ball:
+    def __init__():
+        return
+    
+class Paddle:
+    def __init__(self, x, y, width, height, speed, color):
+        self.posx = x
+        self.posy = y
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = color
+    
+class Snake:
+    def __init__():
+        return
--- Python/snong.py
+++ Python/snong.py
@@ -1,25 +1,22 @@
 import pygame
+from objects import Ball, Paddle, Snake
 
 pygame.init()
 window = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Snong")
 clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        window.fill("black")
 
-    window.fill("black")
 
-    # RENDER YOUR GAME HERE
+        clock.tick(60)  # limits FPS to 60
 
-    # flip() the display to put your work on window
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
+    pygame.quit()
