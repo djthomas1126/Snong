e86d798809f1607bb8b604ee1332cd4eeb18e9f6
Devin <djthomas1126@gmail.com>
2024-01-18 18:24:40 -0600

    Completed basic Pong game

--- Python/objects.py
+++ Python/objects.py
@@ -1,21 +1,85 @@
 import pygame
+import random
+import time
 
 class Ball:
-    def __init__():
-        return
+    def __init__(self, window, x, y, size, speed, color):
+        self.window = window
+        self.posx = x
+        self.posy = y
+        self.width = size   # Both width and height are the same since it is a square
+        self.height = size
+        self.speed = speed
+        self.color = color
+        self.x_dir = random.choice([-1, 1])
+        self.y_dir = random.choice([-1, 1])
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+    def display(self):
+        pygame.draw.rect(self.window, self.color, self.rect)
+        
+    def update(self):
+        self.posx += self.speed * self.x_dir
+        self.posy += self.speed * self.y_dir
+
+        if self.posx <= 0 or self.posx >= (self.window.get_width() - self.width):
+            return 0 # Returns 0  to signal end of round
+        elif self.posy <= 0 or self.posy >= (self.window.get_height() - self.height):
+            self.y_dir *= -1
+
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+        return 1 # Returns 1 to signal the ball is still in play
+
+    def hit(self):
+        self.x_dir *= -1
+
+    def reset(self):
+        w = self.window.get_width()
+        h = self.window.get_height()
+        
+        self.posx = w//2
+        self.posy = h//2
+        self.x_dir = random.choice([-1, 1])
+        self.y_dir = random.choice([-1, 1])
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+        time.sleep(2)
+
+    def getRect(self):
+        return self.rect
     
 class Paddle:
-    def __init__(self, x, y, width, height, speed, color):
+    def __init__(self, window, x, y, width, height, speed, color):
+        self.window = window
         self.posx = x
         self.posy = y
         self.width = width
         self.height = height
         self.speed = speed
         self.color = color
-    
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+    def display(self):
+        pygame.draw.rect(self.window, self.color, self.rect)
+        
+    def update(self, y_dir):
+        self.posy += (self.speed * y_dir)
+        
+        if self.posy <= 0:
+            self.posy = 0
+        elif (self.posy + self.height) >= self.window.get_height():
+            self.posy = self.window.get_height() - self.height
+
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+    def reset(self):
+        h = self.window.get_height()
+        self.posy = h//2
+        self.rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+
+    def getRect(self):
+        return self.rect
+
 class Snake:
     def __init__():
         return
-    
-
- 
--- Python/snong.py
+++ Python/snong.py
@@ -1,19 +1,76 @@
+# Pong code inspired by/derived from: https://www.geeksforgeeks.org/create-a-pong-game-in-python-pygame/#
+
 import pygame
 from objects import Ball, Paddle, Snake
 
 pygame.init()
-window = pygame.display.set_mode((1280, 720))
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+PIXEL_WIDTH = 20
+PADDLE_HEIGHT = 100
+
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Snong")
+
 clock = pygame.time.Clock()
 
 def main():
     running = True
+
+    left_paddle = Paddle(window, 20, (SCREEN_HEIGHT/2 - PADDLE_HEIGHT/2), 
+                         PIXEL_WIDTH, PADDLE_HEIGHT, 10, WHITE)
+    right_paddle = Paddle(window, SCREEN_WIDTH - (PIXEL_WIDTH * 2), 
+                          (SCREEN_HEIGHT/2 - PADDLE_HEIGHT/2), PIXEL_WIDTH, PADDLE_HEIGHT, 10, WHITE)
+
+    left_y_dir = 0
+    right_y_dir = 0
+
+    ball = Ball(window, (SCREEN_WIDTH/2 - PIXEL_WIDTH/2), 
+                (SCREEN_HEIGHT/2 - PIXEL_WIDTH/2), PIXEL_WIDTH, 5, WHITE)
+
     while running:
+        window.fill(BLACK)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        window.fill("black")
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    right_y_dir = -1
+                if event.key == pygame.K_DOWN:
+                    right_y_dir = 1
+                if event.key == pygame.K_w:
+                    left_y_dir = -1
+                if event.key == pygame.K_s:
+                    left_y_dir = 1
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    right_y_dir = 0
+                if event.key == pygame.K_w or event.key == pygame.K_s:
+                    left_y_dir = 0
 
+        if pygame.Rect.colliderect(left_paddle.getRect(), ball.getRect()):
+            ball.hit()
+        elif pygame.Rect.colliderect(right_paddle.getRect(), ball.getRect()):
+            ball.hit()
+        
+        left_paddle.update(left_y_dir)
+        right_paddle.update(right_y_dir)
+        status = ball.update()
+
+        if status == 0:
+            left_paddle.reset()
+            right_paddle.reset()
+            ball.reset()
+
+        left_paddle.display()
+        right_paddle.display()
+        ball.display()
+
+        pygame.display.update()
 
         clock.tick(60)  # limits FPS to 60
 
